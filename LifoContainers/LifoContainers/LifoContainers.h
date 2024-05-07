7146876019cab900b6e2772d3760f9f770b00643
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-08 01:26:39 +0200

    creates an empty project

---
+++ LifoContainers/LifoContainers/LifoContainers.h
@@ -0,0 +1,18 @@
+//
+//  LifoContainers.h
+//  LifoContainers
+//
+//  Created by Alex Levshenkov on 8.05.24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LifoContainers.
+FOUNDATION_EXPORT double LifoContainersVersionNumber;
+
+//! Project version string for LifoContainers.
+FOUNDATION_EXPORT const unsigned char LifoContainersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LifoContainers/PublicHeader.h>
+
+
