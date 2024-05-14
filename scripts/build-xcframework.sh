66bbcbdb94ca02f00274f7f70703637c8263f501
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-15 00:11:36 +0200

    refactoring

--- scripts/build-xcframework.sh
+++ scripts/build-xcframework.sh
@@ -8,23 +8,25 @@ TEMP_ARCHIVE_FOLDER="../archives-temp"
 
 FRAMEWORK_NAME="LifoContainers"
 
-# creating archive for iOS Simulator
+# creating .xcarchive for iOS Simulator
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=iOS Simulator" \
     -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS-Simulator"
 
-# creating archive for iOS
+# creating .xcarchive for iOS
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=iOS" \
     -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS"
 
-# creating archive for macOS
+# creating .xcarchive for macOS
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=macOS" \
     -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-macOS"
+
+# creating .xcframework
