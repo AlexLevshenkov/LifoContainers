1cc5bbc5b47e04fbcd53123d348e871b64a84178
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-14 19:47:21 +0200

    introduces `StackGenericTypeTestCases` class

---
+++ LifoContainers/LifoContainersTests/StacksTests/StackTests/StackGenericTypeTestCases.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import LifoContainers
+
+final class StackGenericTypeTestCases: XCTestCase {
+    func test_init_withDifferentTypes() {
+        _ = Stack<Int>()
+        _ = Stack<Double>()
+        _ = Stack<String>()
+    }
+}
