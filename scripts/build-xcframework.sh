e7535d4d818bfcf4cc7af724d4470516e8b043a6
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-15 00:47:27 +0200

    adds constants for each platform type

--- scripts/build-xcframework.sh
+++ scripts/build-xcframework.sh
@@ -6,27 +6,31 @@ cd "$SCRIPT_FOLDER" || exit
 PROJECT_ROOT_FOLDER="../LifoContainers"
 TEMP_ARCHIVE_FOLDER="../archives-temp"
 
+# names of .xcarchive's
 FRAMEWORK_NAME="LifoContainers"
+IOS_SIMULATOR_ARCHIVE_NAME="${FRAMEWORK_NAME}-iOS-Simulator.xcarchive"
+IOS_ARCHIVE_NAME="${FRAMEWORK_NAME}-iOS.xcarchive"
+MACOS_ARCHIVE_NAME="${FRAMEWORK_NAME}-macOS.xcarchive"
 
 # creating .xcarchive for iOS Simulator
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=iOS Simulator" \
-    -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS-Simulator"
+    -archivePath "${TEMP_ARCHIVE_FOLDER}/${IOS_SIMULATOR_ARCHIVE_NAME}"
 
 # creating .xcarchive for iOS
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=iOS" \
-    -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS"
+    -archivePath "${TEMP_ARCHIVE_FOLDER}/${IOS_ARCHIVE_NAME}"
 
 # creating .xcarchive for macOS
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=macOS" \
-    -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-macOS"
+    -archivePath "${TEMP_ARCHIVE_FOLDER}/${MACOS_ARCHIVE_NAME}"
 
 # creating .xcframework
