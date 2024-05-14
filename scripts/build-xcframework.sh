b9734c9fdd2f4e4d8307f7fe96ee397b0ab532e5
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-15 00:08:26 +0200

    adds commands to archive framework for `iOS Simulator` and `macOS`

--- scripts/build-xcframework.sh
+++ scripts/build-xcframework.sh
@@ -8,9 +8,23 @@ TEMP_ARCHIVE_FOLDER="../archives-temp"
 
 FRAMEWORK_NAME="LifoContainers"
 
+# create archive for iOS Simulator
+xcodebuild archive \
+    -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
+    -scheme "${FRAMEWORK_NAME}" \
+    -destination "generic/platform=iOS Simulator" \
+    -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS-Simulator"
+
 # create archive for iOS
 xcodebuild archive \
     -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
     -scheme "${FRAMEWORK_NAME}" \
     -destination "generic/platform=iOS" \
     -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-iOS"
+
+# create archive for macOS
+xcodebuild archive \
+    -project "${PROJECT_ROOT_FOLDER}/${FRAMEWORK_NAME}.xcodeproj" \
+    -scheme "${FRAMEWORK_NAME}" \
+    -destination "generic/platform=macOS" \
+    -archivePath "${TEMP_ARCHIVE_FOLDER}/${FRAMEWORK_NAME}-macOS"
