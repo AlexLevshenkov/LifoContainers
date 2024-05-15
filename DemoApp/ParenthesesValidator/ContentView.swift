f5ad9b18f58a9183b266f0e680de1dc7129aa091
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-15 16:42:48 +0200

    adds text field

--- DemoApp/ParenthesesValidator/ContentView.swift
+++ DemoApp/ParenthesesValidator/ContentView.swift
@@ -3,12 +3,19 @@ import SwiftUI
 // MARK: - ContentView
 
 struct ContentView: View {
+    @State private var inputText = ""
+
     var body: some View {
         VStack {
             Text("Here you can check your string if parentheses in it are paired! Isn't it cool? 😉")
                 .font(.title)
                 .bold()
                 .multilineTextAlignment(.center)
+
+            Spacer()
+
+            TextField("Try it out!", text: $inputText)
+                .textFieldStyle(.roundedBorder)
 
             Spacer()
         }
