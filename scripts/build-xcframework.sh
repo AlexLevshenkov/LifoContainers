8b9b78ab1c25f7a7a55c42ebe4a795bc6eb22f80
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-15 11:35:54 +0200

    fixes typo

--- scripts/build-xcframework.sh
+++ scripts/build-xcframework.sh
@@ -6,7 +6,7 @@ cd "$SCRIPT_FOLDER" || exit
 # folders
 PROJECT_ROOT_FOLDER="../LifoContainers"
 TEMP_ARCHIVE_FOLDER="../archives-temp"
-OUPUT_FOLDER="../build"
+OUTPUT_FOLDER="../build"
 
 # names of .xcarchive's
 FRAMEWORK_NAME="LifoContainers"
@@ -40,13 +40,13 @@ xcodebuild -create-xcframework \
     -archive "${TEMP_ARCHIVE_FOLDER}/${IOS_SIMULATOR_ARCHIVE_NAME}" -framework "${FRAMEWORK_NAME}.framework" \
     -archive "${TEMP_ARCHIVE_FOLDER}/${IOS_ARCHIVE_NAME}" -framework "${FRAMEWORK_NAME}.framework" \
     -archive "${TEMP_ARCHIVE_FOLDER}/${MACOS_ARCHIVE_NAME}" -framework "${FRAMEWORK_NAME}.framework" \
-    -output "${OUPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework"
+    -output "${OUTPUT_FOLDER}/${FRAMEWORK_NAME}.xcframework"
 
 # creating .zip
-cd "$OUPUT_FOLDER"
+cd "$OUTPUT_FOLDER"
 zip -r "${FRAMEWORK_NAME}.xcframework.zip" "${FRAMEWORK_NAME}.xcframework"
 
 # cleanup
 rm -rf "${TEMP_ARCHIVE_FOLDER}"
 
-open "${OUPUT_FOLDER}"
+open "${OUTPUT_FOLDER}"
