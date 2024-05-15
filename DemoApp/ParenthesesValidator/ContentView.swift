a4a3e79dce2896513074f170fd7056efca594485
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-15 16:39:52 +0200

    adds `MARK` annotations

--- DemoApp/ParenthesesValidator/ContentView.swift
+++ DemoApp/ParenthesesValidator/ContentView.swift
@@ -1,10 +1,14 @@
 import SwiftUI
 
+// MARK: - ContentView
+
 struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
     }
 }
+
+// MARK: - Preview
 
 #Preview {
     ContentView()
