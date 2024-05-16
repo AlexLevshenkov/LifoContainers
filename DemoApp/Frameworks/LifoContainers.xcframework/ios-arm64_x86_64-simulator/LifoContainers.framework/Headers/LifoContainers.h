bef63f4276240f3555d67a1d49cb5e928ca81176
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-16 18:05:25 +0200

    deletes `.xcframework`

--- DemoApp/Frameworks/LifoContainers.xcframework/ios-arm64_x86_64-simulator/LifoContainers.framework/Headers/LifoContainers.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-
-//! Project version number for LifoContainers.
-FOUNDATION_EXPORT double LifoContainersVersionNumber;
-
-//! Project version string for LifoContainers.
-FOUNDATION_EXPORT const unsigned char LifoContainersVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <LifoContainers/PublicHeader.h>
