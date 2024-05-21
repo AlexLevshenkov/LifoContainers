eea1d7243b3130d9a7418d20c2c2766ceb13efdf
Alex Levshenkov <alex.levshenkov.work@gmail.com>
2024-05-21 23:22:32 +0200

    creates `Package.swift` file

---
+++ Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "LifoContainers",
+    platforms: [
+        .macOS(.v10_13),
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "LifoContainers",
+            targets: ["LifoContainers"]),
+    ],
+    targets: [
+        .target(
+            name: "LifoContainers",
+            path: "LifoContainers/LifoContainers/"
+        ),
+        .testTarget(
+            name: "LifoContainersTests",
+            dependencies: ["LifoContainers"],
+            path: "LifoContainers/LifoContainersTests/"
+        ),
+    ]
+)
